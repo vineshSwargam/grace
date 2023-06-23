0b677aed26609ac4c624d45d3209f9d129bf6367
svinesh1729 <svinesh1729@gmail.com>
2023-06-24 03:14:10 +0530

    handle no message state on dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -14,7 +14,7 @@ const page = async ({}) => {
 
   const friends = await getFriendsByUserId(session.user.id)
 
-  const friendsWithLastMessage = await Promise.all(
+  const recentChats = await Promise.all(
     friends.map(async (friend) => {
       const [lastMessageRaw] = (await fetchRedis(
         'zrange',
@@ -23,7 +23,9 @@ const page = async ({}) => {
         -1
       )) as string[]
 
-      const lastMessage = JSON.parse(lastMessageRaw) as Message
+      console.log({ lastMessageRaw });
+
+      const lastMessage = lastMessageRaw ? JSON.parse(lastMessageRaw) as Message : undefined;
 
       return {
         ...friend,
@@ -33,25 +35,22 @@ const page = async ({}) => {
   )
 
   return (
-    <div className='container py-12 px-6'>
-      <h1 className='font-bold text-5xl mb-8'>Recent chats</h1>
-      {friendsWithLastMessage.length === 0 ? (
+    <div className='container'>
+      <h1 className='py-12 px-6 bg-indigo-600 text-white font-bold text-5xl mb-8'>Recent chats</h1>
+      <div className='px-6'>
+      {recentChats.length === 0 ? (
         <p className='text-sm text-zinc-500'>Nothing to show here...</p>
       ) : (
-        friendsWithLastMessage.map((friend) => (
+        recentChats.map((friend) => (
           <div
             key={friend.id}
-            className='relative bg-zinc-50 border border-zinc-200 p-3 rounded-md'>
-            <div className='absolute right-4 inset-y-0 flex items-center'>
-              <ChevronRight className='h-7 w-7 text-zinc-400' />
-            </div>
-
+            className='flex flex-row itemx-center bg-white border border-zinc-400 p-3 rounded-md mb-2'>
             <Link
               href={`/dashboard/chat/${chatHrefConstructor(
                 session.user.id,
                 friend.id
               )}`}
-              className='relative sm:flex'>
+              className='relative flex-grow sm:flex'>
               <div className='mb-4 flex-shrink-0 sm:mb-0 sm:mr-4'>
                 <div className='relative h-6 w-6'>
                   <Image
@@ -64,21 +63,31 @@ const page = async ({}) => {
                 </div>
               </div>
 
-              <div>
+              <div className='flex-grow'>
                 <h4 className='text-lg font-semibold'>{friend.name}</h4>
-                <p className='mt-1 max-w-md'>
-                  <span className='text-zinc-400'>
-                    {friend.lastMessage.senderId === session.user.id
-                      ? 'You: '
-                      : ''}
-                  </span>
-                  {friend.lastMessage.text}
+                {!!friend.lastMessage ? (
+                  <p className='mt-1 w-full text-gray-400'>
+                    <span className='text-zinc-400'>
+                      {friend.lastMessage.senderId === session.user.id
+                        ? 'You: '
+                        : ''}
+                    </span>
+                    {friend.lastMessage.text.substring(0, 50)+'...'}
+                  </p>
+                ) : (
+                  <p className='mt-1 w-full text-gray-400'>
+                  Send a message to start a conversation!
                 </p>
+                )}
               </div>
             </Link>
+            <div className='flex items-center'>
+              <ChevronRight className='h-7 w-7 text-zinc-400' />
+            </div>
           </div>
         ))
-      )}
+        )}
+      </div>
     </div>
   )
 }
